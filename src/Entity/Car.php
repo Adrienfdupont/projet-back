d106ee76f9df417374a069534753831a139244f6
Adrienfdupont <adrienf.dupont@gmail.com>
2023-05-24 15:49:09 +0200

    entities added, relations missing

---
+++ src/Entity/Car.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+class Car
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $serialNumber = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $numberPlate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $colour = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSerialNumber(): ?string
+    {
+        return $this->serialNumber;
+    }
+
+    public function setSerialNumber(string $serialNumber): self
+    {
+        $this->serialNumber = $serialNumber;
+
+        return $this;
+    }
+
+    public function getNumberPlate(): ?string
+    {
+        return $this->numberPlate;
+    }
+
+    public function setNumberPlate(string $numberPlate): self
+    {
+        $this->numberPlate = $numberPlate;
+
+        return $this;
+    }
+
+    public function getColour(): ?string
+    {
+        return $this->colour;
+    }
+
+    public function setColour(string $colour): self
+    {
+        $this->colour = $colour;
+
+        return $this;
+    }
+}
