4762ca9dd5382fc96b70a04fae0b52f6436d44cf
Adrienfdupont <adrienf.dupont@gmail.com>
2023-05-24 16:12:05 +0200

    deleted Model class and added its properties to Car

--- src/Entity/Car.php
+++ src/Entity/Car.php
@@ -22,6 +22,15 @@ class Car
     #[ORM\Column(length: 255)]
     private ?string $colour = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $brand = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -59,6 +68,42 @@ class Car
     public function setColour(string $colour): self
     {
         $this->colour = $colour;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
 
         return $this;
     }
