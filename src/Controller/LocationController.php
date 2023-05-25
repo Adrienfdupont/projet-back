9c48a0da8a9b91ef4e93b2fc40cb577ba1ea05d6
Adrienfdupont <adrienf.dupont@gmail.com>
2023-05-25 11:30:08 +0200

    form works

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -3,37 +3,45 @@
 namespace App\Controller;
 
 use App\Entity\Location;
+use App\Entity\Customer;
+use App\Form\LocationType;
 use App\Repository\CustomerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CarRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class LocationController extends AbstractController
 {
     #[Route('/location', name: 'location')]
-    public function index(CustomerRepository $customerRepository, CarRepository $carRepository): Response
+    public function index(CarRepository $carRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
+        /** @var Customer $user */
         $user = $this->getUser();
 
         if ($user == null) {
             return $this->redirectToRoute('app_login');
         }
-        $customer = $customerRepository->find($user->getId());
 
-        $locations = $customer->getLocations();
-        $cars = $carRepository->findAll();
+        $newLocation = new Location();
+        $newLocation->setCustomer($user);
+        $form = $this->createForm(LocationType::class, $newLocation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $entityManager->persist($newLocation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('location');
+        }
 
         return $this->render('location/index.html.twig', [
-            'locations' => $locations,
-            'cars' => $cars
+            'locations' => $user->getLocations(),
+            'cars' => $carRepository->findAll(),
+            'locationForm' => $form,
         ]);
-    }
-
-    #[Route('/location/form', name: 'form')]
-    public function formAction(Request $request, CustomerRepository $customerRepository, CarRepository $carRepository): Response
-    {
-        dd($request->request->get('startDate'));
     }
 }
